97d933a041ce3a247ba08b58e4fc03562af0998a
Arun Soman <aarunsoman@gmail.com>
2022-10-16 00:22:44 +0530

    testing actions

--- next.config.js
+++ next.config.js
@@ -5,5 +5,31 @@ const nextConfig = {
   images:{
   }
 }
+const repo = 'arunsoman/tooltip-react-tailwind'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
+
+// module.exports = nextConfig
